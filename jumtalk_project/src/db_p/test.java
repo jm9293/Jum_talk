f73d7c65720aacf5cf95861607d8191b64523848
jm97010412 <jm97010412@gmail.com>
2020-06-29 14:42:58 +0900

    test4

--- jumtalk_project/src/db_p/test.java
+++ jumtalk_project/src/db_p/test.java
@@ -4,7 +4,7 @@ public class test {
 
 	public static void main(String[] args) {
 		//¾È³ç¾È³ç¾È³ç////fglsdjfoasjdojf;odsjifjpdjdaofjfajs[osjfijasi/dwdsadsadasdsaasasda
-
+		// ½Ã¹ß Á¿°°Àº ±ê!
 	}
 
 }
