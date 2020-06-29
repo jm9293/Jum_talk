7adee434820c80ffa5c067475cbcd365a13c1fd9
jm97010412 <jm97010412@gmail.com>
2020-06-29 14:51:29 +0900

    test5

--- jumtalk_project/src/db_p/test.java
+++ jumtalk_project/src/db_p/test.java
@@ -3,13 +3,13 @@ package db_p;
 public class test {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		//¾È³ç¾È³ç¾È³ç////fglsdjfoasjdojf;odsjifjpdjdaofjfajs[osjfijasi/dwdsadsadasdsaasasdadsadsadasdawasdasdsddssad
 
-=======
+
 		//¾È³ç¾È³ç¾È³ç////fglsdjfoasjdojf;odsjifjpdjdaofjfajs[osjfijasi/dwdsadsadasdsaasasda
 		// ½Ã¹ß Á¿°°Àº ±ê!
->>>>>>> origin/master
+
 	}
 
 }
